00bce5ebdad0cccfae71dc58628fdab8e253a229
FoxyTony <mirantony@icloud.com>
2021-02-03 15:44:48 +0100

    Implement done, ready for styling

--- cypress/integration/displayTheLocation.feature.js
+++ cypress/integration/displayTheLocation.feature.js
@@ -1,3 +1,32 @@
 describe('Display the location', () => {
-  
-})
+  beforeEach(() => {
+    cy.server()
+    cy.route('GET', 'https://api.opencagedata.com/geocode/v1**', 'fx:virum_location_response.json')
+    cy.route('GET', 'https://api.openweathermap.org/data/**', 'fx:virum_weather_response.json')
+  })
+
+  it('on initial render', () => {
+    cy.visit('/', ({
+      onBeforeLoad(window) {
+        const stubLocation = {
+          coords: {
+            latitude: 55.7842,
+            longitude: 12.4518
+          }
+        };
+        cy.stub(window.navigator.geolocation, "getCurrentPosition").callsFake(
+          callback => {
+            return callback(stubLocation)
+          }
+        )
+      }
+    }))
+    cy.get('[data-cy="weather-display"]').within(()=>{
+      cy.get('[data-cy="location"]').should('contain', 'Virum')
+      cy.get('[data-cy="temp"]').should('contain', "22℃")  
+    }) 
+  });
+});
+
+
+//cy.get('[data-cy=""]') 
--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 class App extends Component {
   state = {
-    location: {}
+    weatherInfo: {}
   }
 
   componentDidMount() {
-    navigator.geolocation.getCurrentPosition(pos => {
-      this.setState({ location: pos.coords })
+    navigator.geolocation.getCurrentPosition(async pos => {
+
+      let { latitude, longitude } = pos.coords
+      const locationResponse = await axios.get(`https://api.opencagedata.com/geocode/v1/json?key=55082dd8fb7e473db8dab86a33518599&q=${latitude}+${longitude}`)
+      const weatherResponse = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=1150a71ab3a6963ec3a8713f33915024&units=metric`)
+      const weatherInfo = {
+        location: locationResponse.data.results[0].components.postal_city,
+        temperature: weatherResponse.data.current.temp
+      }
+      
+      this.setState({ weatherInfo: weatherInfo })
+
+
     })
   }
   render() {
     return (
-      <div>
-        
+      <div data-cy="weather-display">
+        <div data-cy="location">{this.state.weatherInfo.location}</div>
+        <div data-cy="temp">{this.state.weatherInfo.temperature}℃</div>
       </div>
     );
   }
